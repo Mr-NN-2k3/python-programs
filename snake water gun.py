a27149d28f95ace7ab2679bf2c8ba3656372cde0
Mr.NN <73435113+Mr-NN-2k3@users.noreply.github.com>
2024-05-21 11:11:52 +0530

    snake water gun.py

--- snake water gun.py
+++ snake water gun.py
@@ -1,29 +1,35 @@
 import random
+import os
+xx = 0
+db = open("database\snake_water_gun db.txt", "a+")
+#database file location above.
+while (xx != 4):
 
-while True:
-    x = random.choice(["snake", "water", "gun"])
-    xx = input('''1. For snake
-2. For water
-3. For gun 
-4. Exit
-Enter your choice: ''')
-
+    x = random.choices(["snake", "water", "gun"], k=1)[0]
+    xx = input(
+        "1.for the snake. \n2.for the water. \n3.for the gun. \n4.exit. \nenter the input for the game :")
+    
     if xx == "4":
         print("Exiting the game. Thanks for playing!")
         break
-    elif xx not in ["1", "2", "3"]:
-        print("Invalid input, please try again.")
+    if xx not in ["1", "2", "3"]:
+        print("this is invalid input")
         continue
-    elif xx == "1":
-        xx = "snake"
-    elif xx == "2":
-        xx = "water"
-    elif xx == "3":
-        xx = "gun"
-
-    if (xx == "snake" and x == "water") or (xx == "water" and x == "gun") or (xx == "gun" and x == "snake"):
-        print("\nComputer wins. You chose:", xx, "\nComputer chose:", x , "\n\n\n")
-    elif (xx == "snake" and x == "snake") or (xx == "water" and x == "water") or (xx == "gun" and x == "gun"):
-        print("\nYou win! You chose:", xx, "\nComputer chose:", x, "\n\n\n")
+    #input from the user
+    
+    if (xx == "1" and x == "water") or (xx == "2" and x == "gun") or (xx == "1" and x == "gun"):
+        print("computer wins by having=", x, "human=", xx)
+        db.write(
+            f"\n\ncomputer WIN by having '{x}'\nwhile human LOST by having '{xx}'")
+    #win logic for computer.
+    
+    elif (xx == "1" and x == "snake") or (xx == "2" and x == "water") or (xx == "3" and x == "gun"):
+        print("Human wins the game by having:", xx, "Computer:", x)
+        db.write(
+            f"\n\ncomputer LOST by having '{x}'\nwhile human WINS by having '{xx}'")
+    #win logc for the human.
     else:
-        print("\nIt's a draw! You both chose:", xx , "\n\n\n")
+        print("its a tie between human and the computer")
+        db.write(
+            f"\n\nIt was a TIE between the Computer and Human both choose '{x}'")
+    #tie logic for the both
