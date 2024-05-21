fe230cdd246ba9ebafd41f0e194133b0d399fbc7
Mr.NN <73435113+Mr-NN-2k3@users.noreply.github.com>
2024-05-21 10:51:00 +0530

    guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -1,21 +1,32 @@
 import random
+import os
 
-x = range(1 , 10)
+f = open("D:\Computer Programs\Python\database\guessing_gamedb.txt", "a+")
+#input your desired file location above.
+
+var = random.choice(range(0, 10))
 i = 0
+play = int(input(
+    "To play the game press => 1\nTo exit the game press => 2\nYour selection = "))
+while (play == 1):
+    for x in range(3):
 
-var = random.sample(x , k = 1)
+        input1 = int(input("enter your guess => "))
 
-for var in range(i):
-    input1 = int(input("enter your guess => "))
-    if input1 == var:
-        print("you won the game.")
-        break
-        i+=1
+        if input1 == var:
+            f.write("win"+"\n")
+            print("you won the game.")
+            break
+        # Winning logic here
         
-    if i==3:
-        print("plz try again you have used " , i , "no of chances from 3 ")
+        i = i+1
+        if i <= 2:
+            print("plz try again you have used ", i, "no of chances from 3 ")
+        #Number of the chances to display.
+        else:
+            print("\n\nyou lost the game. \nThe correct number was  ", var)
+            f.write("lost" + "\n")
+        #losing logic here.
 
-else:
-    print("you lost the game")
-
-
+print("Exiting the game.\nSee You Again")
+#the exiting the game logic.
